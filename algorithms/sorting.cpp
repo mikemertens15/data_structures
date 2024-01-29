12fc0f99c96f60136b4c91971b1816beb295fdae
Mike Mertens <mikemertens15@gmail.com>
2024-01-28 22:18:15 -0500

    Add sorting algorithms implementation

---
+++ algorithms/sorting.cpp
@@ -0,0 +1,28 @@
+#include "../include/Dynamic_Array.h"
+
+namespace sorting {
+    template <typename T>
+    void bubble_sort(Dynamic_Array<T> &arr) {
+
+    }
+
+    template <typename T>
+    void selection_sort(Dynamic_Array<T> &arr) {
+
+    }
+
+    template <typename T>
+    void insertion_sort(Dynamic_Array<T> &arr) {
+
+    }
+
+    template <typename T>
+    void merge_sort(Dynamic_Array<T> &arr) {
+
+    }
+
+    template <typename T>
+    void quick_sort(Dynamic_Array<T> &arr) {
+        
+    }
+}
